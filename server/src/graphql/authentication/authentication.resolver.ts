96a5ff812b7e1f696d1f826d19c07aa62cf236ce
lubimov.v <lubimov.work@gmail.com>
2021-08-08 10:14:48 +0300

    Add login action

--- server/src/graphql/authentication/authentication.resolver.ts
+++ server/src/graphql/authentication/authentication.resolver.ts
@@ -1,4 +1,9 @@
 @Resolver('auth')
 export class AuthenticationResolver {
+    constructor(private authService: AuthService) {}
 
+    @Query(() => [UserDto])
+    async login(): Promise<UserDto> {
+        return this.authService.login();
+    }
 }
